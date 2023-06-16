8a0ead0edf888c58ad1e48af9cc423d5e0fb5139
Amin <price1375@yahoo.com>
2023-06-16 17:04:30 +0330

    cors error handling

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -27,6 +27,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = __importStar(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const router_1 = __importDefault(require("./router/router"));
 const mongoose_1 = __importDefault(require("mongoose"));
@@ -35,8 +36,15 @@ const app = (0, express_1.default)();
 dotenv.config();
 mongoose_1.default.connect('mongodb://127.0.0.1:27017/Shopping-project')
     .then(() => console.log("Connected to DB!"));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded());
 app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 app.use("/", router_1.default);
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use((0, cors_1.default)(corsOptions));
 app.listen("3000", () => console.log("Server run on port 3000"));
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import cors from "cors";
 import express from "express"
 import router from "./router/router"
 import mongoose from "mongoose";
@@ -8,11 +9,19 @@ dotenv.config();
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/Shopping-project')
-  .then((): void => console.log("Connected to DB!"));
+.then((): void => console.log("Connected to DB!"));
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")))
 app.use("/", router);
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.listen("3000", (): void => console.log("Server run on port 3000"));
